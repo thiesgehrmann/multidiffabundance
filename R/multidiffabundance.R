c8b9d027e9a95984f45134884b833411adf58e74
Tim Van Rillaer <tim.van.rillaer@hotmail.com>
2023-10-16 16:55:25 +0200

    Add multidiffabundance.R file

---
+++ R/multidiffabundance.R
@@ -0,0 +1,14 @@
+#' multidiffabundance: A toolkit for the testing of differential abundance with many different tools, each provided wit>#'
+#' multidiffabundance (MDA) is an R package for differnetial abundance testing with many different tools.
+#' The following packages are currently supported
+#' (Only tools that allow for adjustment with other variables are selected):
+#' ALDEx2, ANCOMBC, Corncob, DESeq2, Limma(voom), lm/lmer CLR, and Maaslin2.
+#'
+#' @docType package
+#' @name multidiffabundance
+#'
+#' @section Author(s):
+#' Thies Gehrmann \email{thies.gehrman@@uantwerpen.be}
+#'
+NULL
+
