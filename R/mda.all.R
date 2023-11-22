0fff535e680fe4de70eb7dc91d59372184a40373
Thies Gehrmann <thiesgehrmann@gmail.com>
2023-11-22 13:04:16 +0100

    Updated all and deseq, and also the shell wrapper (summary still not working)

--- R/mda.all.R
+++ R/mda.all.R
@@ -1,12 +1,12 @@
 
 mda.all <- function(mda.D, alpha=FALSE, beta=FALSE, group=FALSE, continuous=FALSE,
-                    aldex2=TRUE, ancombc=TRUE, corncob=FALSE, deseq2=TRUE,
+                    aldex2=TRUE, ancombc2=TRUE, corncob=FALSE, deseq2=TRUE,
                     limma=TRUE, lmclr=TRUE, maaslin2=TRUE, zicoseq=TRUE, ...){
     suppressPackageStartupMessages(require(dplyr))
     D <- mda.D
     
     functions <- c(mda.alpha, mda.beta, mda.group, mda.continuous, mda.aldex2, mda.ancombc2, mda.corncob, mda.deseq2, mda.limma, mda.lmclr, mda.maaslin2, mda.zicoseq)
-    selected <-  c(    alpha,     beta,     group,     continuous,     aldex2,     ancombc,      corncob,     deseq2,     limma,     lmclr,     maaslin2,     zicoseq)
+    selected <-  c(    alpha,     beta,     group,     continuous,     aldex2,     ancombc2,      corncob,     deseq2,     limma,     lmclr,     maaslin2,     zicoseq)
     
     R <- lapply(functions[selected], function(x){ x(D, ...) })
     res <- bind_rows(lapply(R, function(x){x$res}))
