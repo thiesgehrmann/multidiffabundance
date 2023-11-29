ea43304cb919792bc88f509594f0c6df31732433
Thies Gehrmann <thiesgehrmann@gmail.com>
2023-11-29 15:26:25 +0100

    Updated alpha function to permit additional diversity indices with alpha.index.

--- R/mda.alpha.R
+++ R/mda.alpha.R
@@ -1,4 +1,4 @@
-mda.alpha <- function(mda.D, ...){
+mda.alpha <- function(mda.D, alpha.index=c("shannon"), ...){
     D <- mda.D
     
     suppressPackageStartupMessages({
@@ -7,9 +7,16 @@ mda.alpha <- function(mda.D, ...){
         require(vegan)
         require(lmerTest)})
     
-    alpha_measure <- scale(diversity(D$count_data))
+    index <- intersect(alpha.index, c("shannon", "simpson", "invsimpson"))
+    
+    if(length(index) == 0){
+        message("[MDA] mda.alpha: No valid index value(s) defined. Options must be [shannon, simpson, invsimpson].")
+        exit(0)
+    }
+    
+    alpha_indices <- lapply(index, function(i){scale(diversity(D$count_data, index=i))})
 
-    alpha <- function(f_idx){
+    alpha <- function(f_idx, i_idx){
         fdata <- D$formula[[f_idx]]
         f <- fdata$fn
 
@@ -18,11 +25,14 @@ mda.alpha <- function(mda.D, ...){
         } else { lm }
         
         meta_data <- data.frame(fdata$data)
-        meta_data$mda.alpha <- alpha_measure
+        
+
+        meta_data$mda.alpha <- alpha_indices[[i_idx]]
+        taxa_name <- paste0(c("mda","alpha",index[i_idx]), collapse='.')
 
         f <- update(f, mda.alpha ~ .)
-        
-        r <- mda.trycatchempty(D, f_idx, method(f, data=meta_data, na.action = 'na.exclude'), taxa="mda.alpha.shannon")
+
+        r <- mda.trycatchempty(D, f_idx, method(f, data=meta_data, na.action = 'na.exclude'), taxa=taxa_name)
 
         if (r$error){
             return(r$response)
@@ -30,12 +40,13 @@ mda.alpha <- function(mda.D, ...){
         fit <- r$response
 
         s <- as.data.frame(coefficients(summary(fit)))
-        
+
         if (mda.isSingular(fit)){
             s[,"Pr(>|t|)"] <- NA
             s[,"comment"] <- paste0(c("Rank deficient: singular.", r$message), collapse='\n')
         }
-        s$taxa <- c("mda.alpha")
+        
+        s$taxa <- taxa_name
         s <- s %>% rownames_to_column("variable.mda")
         res <- s
 
@@ -48,7 +59,11 @@ mda.alpha <- function(mda.D, ...){
     }
 
     do <- function(f_idx){
-        res.full <- mda.cache_load_or_run_save(D, f_idx, "alpha", {alpha(f_idx)})
+        res.full <- lapply(1:length(index), function(i_idx){
+            m <- paste(c("alpha",index[i_idx]), collapse=".")
+            mda.cache_load_or_run_save(D, f_idx, m, {alpha(f_idx, i_idx)})
+            })
+        res.full <- bind_rows(res.full)
         mda.common_do(D, f_idx, res.full, "alpha", skip_taxa_sel=TRUE)
     }
 
