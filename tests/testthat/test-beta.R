876fe3e8ca47b5b0afefc3fb3b8021bd7172d950
TheOaphidian <tim.van.rillaer@hotmail.com>
2025-03-25 14:01:44 +0100

    Limit beta test to 2 cpus

--- tests/testthat/test-beta.R
+++ tests/testthat/test-beta.R
@@ -1,5 +1,17 @@
+# devtools::check fails on requesting >2 cores
+# so we provide a switch here
+chk <- Sys.getenv("_R_CHECK_LIMIT_CORES_", "")
+
+if (nzchar(chk) && chk == "TRUE") {
+    # use 2 cores in CRAN/Travis/AppVeyor
+    num_workers <- 2L
+} else {
+    # use all cores in devtools::test()
+    num_workers <- parallel::detectCores()
+}
+
 test_that("mda.beta works", {
   expect_no_error({
-    suppressMessages(mda.beta(D))
+    suppressMessages(mda.beta(D, parallel=num_workers))
     })
 })
