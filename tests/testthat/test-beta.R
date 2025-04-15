77f2bc3818d1ef0006bc61d127574dd142c992ed
Tim Van Rillaer <tim.van.rillaer@hotmail.com>
2025-04-15 12:29:58 +0200

    Update test-beta.R

--- tests/testthat/test-beta.R
+++ tests/testthat/test-beta.R
@@ -1,17 +1,5 @@
-# devtools::check fails on requesting >2 cores
-# so we provide a switch here
-chk <- Sys.getenv("_R_CHECK_LIMIT_CORES_", "")
-
-if (nzchar(chk) && chk == "TRUE") {
-    # use 1 core, we can't have nice things here
-    num_workers <- 1L
-} else {
-    # use all cores in devtools::test()
-    num_workers <- parallel::detectCores()
-}
-
 test_that("mda.beta works", {
   expect_no_error({
-    suppressMessages(mda.beta(D, beta.parallel = num_workers))
+    suppressMessages(mda.beta(D, beta.parallel = 1))
     })
 })
